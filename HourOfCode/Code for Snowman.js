415d95195dc533afe9db303ca9f2dad3473e8973
Nikita Yovchev <31662427+nikitayvchv@users.noreply.github.com>
2017-10-12 10:01:53 -0400

    Add files via upload

---
+++ HourOfCode/Code for Snowman.js
@@ -0,0 +1,31 @@
+//Code for snowman project (khan academy)
+//sky
+background(213, 248, 250);
+
+//ground
+rect(-1, 362, 402, 55);
+
+// simple snowman
+ellipse(200, 300, 150, 150);
+ellipse(200, 200, 100, 100);
+ellipse(200, 120, 75, 75);
+
+//hat
+fill(0, 0, 0);
+rect(164, 74, 71, 11);
+rect(174, 76, 51, -42);
+fill(255, 255, 255);
+rect(174, 62, 51, 11);
+
+//eyes
+fill(46, 46, 46);
+ellipse(187, 118, 10, 10);
+ellipse(213, 120, 10, 10);
+
+//arms
+stroke(140, 56, 0);
+line(156,176,101,202);
+line(86,127,101,202);
+line(307,198,245,178);
+line(308,198,283,248);
+
---
+++ HourOfCode/README.md
@@ -0,0 +1,11 @@
+### HourOfCode
+
+This is a repository for the "_Hour of Code_" on [KhanAcademy](https://www.khanacademy.org/).
+It's a learning tool to teach begginers the basics of coding with _JavaScript_.
+
+To do the Hour of Code, click [here](https://www.khanacademy.org/computing/hour-of-code/hour-of-drawing-code/a/how-we-teach-coding-on-ka).
+
+**Nikita Yovchev
+9/7/2017
+A.Y. Jackson
+ICS4U**
